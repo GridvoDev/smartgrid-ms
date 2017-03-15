a8626313e8b9256f3a5949d5fd9bb049cf362913
patrick176 <huangjc@refinece.com>
2017-03-15 17:37:57 +0800

    k8s modified

--- k8s/dev/install.sh
+++ k8s/dev/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-kubectl get svc | grep -q smartgrid-ms
+kubectl -n gridvo get svc | grep -q smartgrid-ms
 if [ "$?" == "1" ];then
 	kubectl create -f smartgrid_ms-service.yaml --record
-	kubectl get svc | grep -q smartgrid-ms
+	kubectl -n gridvo get svc | grep -q smartgrid-ms
 	if [ "$?" == "0" ];then
 		echo "smartgrid_ms-service install success!"
 	else
@@ -11,10 +11,10 @@ if [ "$?" == "1" ];then
 else
 	echo "smartgrid_ms-service is exist!"
 fi
-kubectl get pods | grep -q smartgrid-ms
+kubectl -n gridvo get pods | grep -q smartgrid-ms
 if [ "$?" == "1" ];then
 	kubectl create -f smartgrid_ms-deployment.yaml --record
-	kubectl get pods | grep -q smartgrid-ms
+	kubectl -n gridvo get pods | grep -q smartgrid-ms
 	if [ "$?" == "0" ];then
 		echo "smartgrid_ms-deployment install success!"
 	else
@@ -22,13 +22,13 @@ if [ "$?" == "1" ];then
 	fi
 else
 	kubectl delete -f smartgrid_ms-deployment.yaml
-	kubectl get pods | grep -q smartgrid-ms
+	kubectl -n gridvo get pods | grep -q smartgrid-ms
 	while [ "$?" == "0" ]
 	do
-	kubectl get pods | grep -q smartgrid-ms
+	kubectl -n gridvo get pods | grep -q smartgrid-ms
 	done
 	kubectl create -f smartgrid_ms-deployment.yaml --record
-	kubectl get pods | grep -q smartgrid-ms
+	kubectl -n gridvo get pods | grep -q smartgrid-ms
 	if [ "$?" == "0" ];then
 		echo "smartgrid_ms-deployment update success!"
 	else
